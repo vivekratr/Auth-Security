2f43bb46cf1766e8245d18f63d2f07d3f4be26a6
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2023-10-04 12:03:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({  extended: true })); 
 
 mongoose.connect("mongodb://localhost:27017/levleOneDB", {
   useNewUrlParser: true,
